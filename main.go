d08520984a850d129973215963990d80e0bf997b
Nils Lagerkvist <nils.lagerkvist@gmail.com>
2016-10-04 21:22:31 +0200

    Cleaned up logging, help messages and code

--- main.go
+++ main.go
@@ -25,28 +25,54 @@ import (
 const (
 	// flagFileName is the configuration file used for sshauth
 	flagFileName = "/etc/sshauth/sshauth.conf"
-
-	logheader = "command=\"%s %s %s %s\" "
+	syslogTag    = "sshauth"
+	logheader    = "command=\"%s %s %s %s\" "
 )
 
 var (
-	bucket            = flag.String("bucket", "", "S3 bucket `name`")
-	key               = flag.String("key", "", "S3 bucket `prefix`")
-	region            = flag.String("region", "", "AWS `region`, eg. eu-west-1")
-	authlog           = flag.String("authlog", "", "Set `path` to sshlogger script")
-	syslogEnabled     = flag.Bool("syslog", false, "Enable logging via syslog")
-	printSSHLogger    = flag.Bool("sshlogger", false, "Print sshlogger script to stdout")
-	enableDebugOutput = flag.Bool("debug", false, "Enable debug output")
+	bucket         = flag.String("bucket", "", "S3 bucket `name`")
+	key            = flag.String("key", "", "S3 bucket `prefix`")
+	region         = flag.String("region", "", "AWS `region`, eg. eu-west-1")
+	authlog        = flag.String("authlog", "", "Set `path` to sshlogger script")
+	syslogEnabled  = flag.Bool("syslog", false, "Enable logging via syslog")
+	printSSHLogger = flag.Bool("sshlogger", false, "Print sshlogger script to stdout and exit")
+	enableLogging  = flag.Bool("logging", true, "Set to false to disable logging")
+	enableDebugLog = flag.Bool("debug", false, "Enable debug output")
 
+	info  = log.New(ioutil.Discard, "", 0)
 	debug = log.New(ioutil.Discard, " * ", 0)
 
 	svc = s3.New(nil)
 )
 
 func usage() {
-	fmt.Fprintln(os.Stderr, "Usage: sshauth [OPTIONS] <username>\n\nOptions:")
+	fmt.Fprintln(os.Stderr,
+		`Usage: sshauth [OPTIONS] -bucket <name> [-key <prefix>] <username>
+Read authorized keys from S3 to be used with AuthorizedKeysCommand in sshd
+`)
+
 	flag.PrintDefaults()
-	fmt.Fprintln(os.Stderr, "\nThe final S3 URL will be: bucket/prefix/username")
+
+	fmt.Fprintln(os.Stderr, `
+Note: The final S3 path will be: s3://bucket/key/username
+
+CONFIGURATION
+Default configuration is done by defining flags in /etc/sshauth/sshauth.conf
+That is, in the same way as done on the command line.
+
+AUTHLOG
+When the authlog feature is enabled sshauth will inject a command option for
+each authorized key. The command will be the one specified by the -authlog flag.
+The command is provided three commmand line parameters: ´user´, ´bucket´, and
+´key´. In addition, the command originally supplied by the client is available
+in the SSH_ORIGINAL_COMMAND environment variable.
+
+Create sshlogger.sh
+´sshauth -sshlogger > /usr/local/bin/sshlogger.sh´
+
+Running sshauth with authlog enabled
+´sshauth -bucket myBucket -authlog /usr/local/bin/sshlogger.sh myUser´
+`)
 }
 
 func usageError(error string) {
@@ -55,35 +81,32 @@ func usageError(error string) {
 	os.Exit(1)
 }
 
-func init() {
-	flag.Usage = usage
-}
-
 func main() {
-	log.SetFlags(0)
+
+	// Disable timestamp on log messages
+	info.SetFlags(0)
+
+	// Read flag file with default configuration
 	readDefaultFlagFile()
+
+	flag.Usage = usage
 	flag.Parse()
 
 	// setup normal logger
-	if *syslogEnabled {
-		var err error
-		syslogWriter, err := syslog.New(syslog.LOG_NOTICE, "sshauth")
-		if err != nil {
-			log.Fatalf("unable to initialize sysloger: %v", err)
+	if *enableLogging {
+		info.SetOutput(os.Stderr)
+
+		if *syslogEnabled {
+			mustEnableSyslog(info, syslog.LOG_NOTICE, syslogTag)
 		}
-		log.SetOutput(syslogWriter)
 	}
 
-	//
-	if *enableDebugOutput {
+	// setup debug logging
+	if *enableLogging && *enableDebugLog {
 		debug.SetOutput(os.Stderr)
 
 		if *syslogEnabled {
-			syslogWriter, err := syslog.New(syslog.LOG_INFO, "sshauth")
-			if err != nil {
-				log.Fatalf("unable to setup syslogger: %v", err)
-			}
-			debug.SetOutput(syslogWriter)
+			mustEnableSyslog(debug, syslog.LOG_INFO, syslogTag)
 		}
 	}
 
@@ -113,15 +136,24 @@ func main() {
 	printAuthorizedKeys(*bucket, *key, user)
 }
 
+func mustEnableSyslog(logger *log.Logger, p syslog.Priority, tag string) {
+	syslogWriter, err := syslog.New(p, tag)
+	if err != nil {
+		info.Fatalf("unable to initialize sysloger: %v", err)
+	}
+	logger.SetOutput(syslogWriter)
+}
+
 func listenOnSigpipe() {
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, syscall.SIGPIPE)
 	<-c
-	debug.Println("Got SIGPIPE signal")
+	debug.Printf("recived SIGPIPE signal: ignoring")
 }
 
 // readAuthorizedKey reads the authorized keys from S3
 func readAuthorizedKey(bucket, key string, authorizedKeys chan io.Reader) {
+	debug.Printf("Reading authorized key from bucket: %s, path: %s", bucket, key)
 	params := &s3.GetObjectInput{
 		Bucket: aws.String(bucket),
 		Key:    aws.String(key),
@@ -131,9 +163,9 @@ func readAuthorizedKey(bucket, key string, authorizedKeys chan io.Reader) {
 	if err != nil {
 		switch e := err.(type) {
 		case awserr.Error:
-			debug.Printf("unable to get authorized key from S3: %s: %s", e.Code(), e.Message())
+			info.Printf("Unable to get authorized key from S3: %s: %s", e.Code(), e.Message())
 		default:
-			debug.Printf("unable to get authorized key from S3: %v", e)
+			info.Printf("Unable to get authorized key from S3: %v", e)
 		}
 		authorizedKeys <- bytes.NewReader([]byte{})
 		return
@@ -155,10 +187,10 @@ func printAuthorizedKeys(bucket, authorizedKeysPath, user string) {
 	authorizedKeys := make(chan io.Reader, 5)
 
 	authorizedKeysPath = path.Join(authorizedKeysPath, user)
-	debug.Printf("listing authorized keys in bucket: %s, path: %s", bucket, authorizedKeysPath)
+	debug.Printf("Listing authorized keys from bucket: %s, path: %s", bucket, authorizedKeysPath)
 
 	params := &s3.ListObjectsInput{
-		Bucket: aws.String(bucket), // Required
+		Bucket: aws.String(bucket),
 		Prefix: aws.String(authorizedKeysPath),
 	}
 
@@ -179,7 +211,7 @@ func printAuthorizedKeys(bucket, authorizedKeysPath, user string) {
 					// Expected error
 					return false
 				}
-				log.Println("Unable to copy authorized key to stdout:", err)
+				info.Printf("Unable to copy authorized key to stdout: %v", err)
 			}
 		}
 
@@ -189,9 +221,9 @@ func printAuthorizedKeys(bucket, authorizedKeysPath, user string) {
 	if err != nil {
 		switch e := err.(type) {
 		case awserr.Error:
-			log.Fatalf("unable to list authorized keys: %s, message: %s", e.Code(), e.Message())
+			info.Fatalf("Unable to list authorized keys: %s, message: %s", e.Code(), e.Message())
 		default:
-			log.Fatal("Error:", e)
+			info.Fatalf("Error listing authorized keys: %v", e)
 		}
 	}
 }
@@ -206,30 +238,30 @@ func readDefaultFlagFile() {
 // those flags will override. Note, if flags are read into an array they
 // will not be overridden, but appended.
 func readFlagFile(flagFileName string) {
+	debug.Printf("Reading flag file: %s", flagFileName)
+
 	flagFile, err := os.Open(flagFileName)
 	if err != nil {
 		dir, _ := os.Getwd()
-		debug.Println("Unable to open file: ", flagFileName, ", In folder:", dir)
+		debug.Printf("Unable to open file: %s, directory: %s", flagFileName, dir)
 		return
 	}
 	defer flagFile.Close()
 
-	debug.Println("Reading flag file:", flagFileName)
-
-	var newArgs []string
-	newArgs = append(newArgs, os.Args[0])
+	var args []string
+	args = append(args, os.Args[0])
 
 	// Read arguments from file
 	scanner := bufio.NewScanner(flagFile)
 	scanner.Split(bufio.ScanWords)
 	for scanner.Scan() {
-		newArgs = append(newArgs, scanner.Text())
+		args = append(args, scanner.Text())
 	}
 
 	// Add arguments from command line after
 	for i := 1; i < len(os.Args); i++ {
-		newArgs = append(newArgs, os.Args[i])
+		args = append(args, os.Args[i])
 	}
 
-	os.Args = newArgs
+	os.Args = args
 }
